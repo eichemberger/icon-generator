299d14cc0172d76b5ef497de3094c915f3b464e6
German Eichemberger <germaneichemberger@gmail.com>
2023-08-04 23:54:07 +0100

    creates button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Button(props: React.ComponentPropsWithoutRef<'button'>) {
+    return <button {...props} className="bg-blue-400 hover:bg-blue-500 px-4 py-2 rounded">{props.children}</button>;
+}
