daf1f7969b202021c36971fa29b3816bfed5708d
German Eichemberger <germaneichemberger@gmail.com>
2023-08-04 23:03:00 +0100

    adds first mutation ednpoint

---
+++ src/server/api/routers/generate.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+import {
+    createTRPCRouter,
+    publicProcedure,
+    protectedProcedure,
+} from "~/server/api/trpc";
+
+export const generateRouter = createTRPCRouter({
+    generateIcon: publicProcedure.input(z.object({
+        prompt: z.string()
+    })).mutation(({input}) => {
+        console.log('we are here', input.prompt);
+        return {
+            message: 'success'
+        }
+    })
+});
