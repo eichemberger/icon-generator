89b61d6cbe2d0a95aad06acc78ed6a5a31e4ebee
German Eichemberger <germaneichemberger@gmail.com>
2023-08-05 23:15:37 +0100

    updates prompt

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -44,10 +44,12 @@ async function generateIcon(prompt: string): Promise<string> {
 
 export const generateRouter = createTRPCRouter({
     generateIcon: protectedProcedure.input(z.object({
-        prompt: z.string()
+        prompt: z.string(),
+        color: z.string(),
+        shape: z.string(),
+        style: z.string(),
+        numberOfIcons: z.number().min(1).max(10),
     })).mutation(async ({ctx, input}) => {
-        console.log('we are here', input.prompt);
-
         const {count} = await ctx.prisma.user.updateMany({
             where: {
                 id: ctx.session.user.id,
@@ -68,14 +70,17 @@ export const generateRouter = createTRPCRouter({
                 message: 'you do not have enough credits'
             });
         }
-        const base64EncodedImage = await generateIcon(input.prompt);
+
+        const finalPrompt = `a modern ${input.shape} icon in ${input.color} of ${input.prompt}, ${input.style}, minimialistic, high quality, trending on art station, unreal engine graphics quality`;
+
+        const base64EncodedImage = await generateIcon(finalPrompt);
 
         const icon = await ctx.prisma.icon.create({
             data: {
                 prompt: input.prompt,
                 userId: ctx.session.user.id
             }
-        })
+        });
 
         await s3.putObject({
             Bucket: BUCKET_NAME,
